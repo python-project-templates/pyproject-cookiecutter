6ee55bf9b29b436e3ee19c5c7884494c67b51d23
Tim Paine <t.paine154@gmail.com>
2022-02-25 12:04:21 -0500

    Update setup.py

--- {{cookiecutter.extension_name}}/setup.py
+++ {{cookiecutter.extension_name}}/setup.py
@@ -3,86 +3,94 @@ from codecs import open
 from os import path
 
 from jupyter_packaging import (
-    create_cmdclass, install_npm, ensure_targets,
-    combine_commands, ensure_python, get_version
+    create_cmdclass,
+    install_npm,
+    ensure_targets,
+    combine_commands,
+    get_version,
 )
+
 pjoin = path.join
 
-ensure_python(('2.7', '>=3.7'))
+name = "{{ cookiecutter.extension_name }}"
 here = path.abspath(path.dirname(__file__))
-jshere = path.abspath(pjoin(path.dirname(__file__), 'js'))
-name = '{{ cookiecutter.extension_name }}'
-version = get_version(pjoin(here, name, '_version.py'))
+jshere = path.abspath(path.join(here, "js"))
+version = get_version(pjoin(here, name, "_version.py"))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read().replace("\r\n", "\n")
+
+requires = ["jupyterlab>=3.0.0", "notebook>=6.0.3"]
+
+requires_dev = requires + [
+    "black>=20.",
+    "bump2version>=1.0.0",
+    "flake8>=3.7.8",
+    "flake8-black>=0.2.1",
+    "jupyter_packaging",
+    "mock",
+    "pytest>=4.3.0",
+    "pytest-cov>=2.6.1",
+    "Sphinx>=1.8.4",
+    "sphinx-markdown-builder>=0.5.2",
+]
+
 
 data_spec = [
     # Lab extension installed by default:
-    ('share/jupyter/lab/extensions',
-     'lab-dist',
-     'jupyterlab_autoversion-*.tgz'),
+    (
+        "share/jupyter/labextensions/jupyterlab_commands",
+        "jupyterlab_commands/labextension",
+        "**",
+    ),
     # Config to enable server extension by default:
-    ('etc/jupyter',
-     'jupyter-config',
-     '**/*.json'),
+    ("etc/jupyter/jupyter_server_config.d", "jupyter-config", "*.json"),
 ]
 
 
-cmdclass = create_cmdclass('js', data_files_spec=data_spec)
-cmdclass['js'] = combine_commands(
-    install_npm(jshere, build_cmd='build:all'),
-    ensure_targets([
-        pjoin(jshere, 'lib', 'index.js'),
-        pjoin(jshere, 'style', 'index.css')
-    ]),
+cmdclass = create_cmdclass("js", data_files_spec=data_spec)
+cmdclass["js"] = combine_commands(
+    install_npm(jshere, build_cmd="build:all"),
+    ensure_targets(
+        [
+            pjoin(jshere, "lib", "index.js"),
+            pjoin(jshere, "style", "index.css"),
+            pjoin(here, "jupyterlab_commands", "labextension", "package.json"),
+        ]
+    ),
 )
-
-requires = [
-]
-
-requires_dev = [
-    'bump2version',
-    'flake8>=3.7.8',
-    'mock',
-    'pybind11>=2.4.0',
-    'pytest>=4.3.0',
-    'pytest-cov>=2.6.1',
-    'Sphinx>=1.8.4',
-    'sphinx-markdown-builder>=0.5.2',
-] + requires
 
 
 setup(
     name=name,
     version=version,
-    description='{{ cookiecutter.project_short_description }}',
+    description="{{ cookiecutter.project_short_description }}",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url='{{REPO_URL}}',
-    author='Tim Paine',
-    author_email='t.paine154@gmail.com',
-    license='Apache 2.0',
-
+    author="Tim Paine",
+    author_email="t.paine154@gmail.com",
+    license="Apache 2.0",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Framework :: Jupyter",
+        "Framework :: Jupyter :: JupyterLab",
     ],
-
     cmdclass=cmdclass,
-    keywords='',
-    packages=find_packages(exclude=['tests', ]),
-    install_requires=requires,
-    extras_require={
-        'dev': requires_dev,
-    },
-    include_package_data=True,
+    keywords="jupyter jupyterlab",
+    packages=find_packages(
+        exclude=[
+            "tests",
+        ]
+    ),
     zip_safe=False,
+    extras_require={
+        "dev": requires_dev,
+    },
+    python_requires=">=3.7",
 )
