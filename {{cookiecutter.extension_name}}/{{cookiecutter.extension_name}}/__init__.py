1d09cda2f6d64a26cd9eb558166bfbe371435074
Tim Paine <t.paine154@gmail.com>
2022-02-25 12:06:00 -0500

    Update __init__.py

--- {{cookiecutter.extension_name}}/{{cookiecutter.extension_name}}/__init__.py
+++ {{cookiecutter.extension_name}}/{{cookiecutter.extension_name}}/__init__.py
@@ -1,1 +1,5 @@
-from ._version import __version__  # noqa: F401
+from ._version import __version__
+
+
+def _jupyter_server_extension_paths():
+    return [{"module": "jupyterlab_commands.extension"}]
